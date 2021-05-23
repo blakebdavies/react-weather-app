e370931905e23378afd0d75d4ffd8d3fd7855066
blakebdavies <81705490+blakebdavies@users.noreply.github.com>
2021-05-23 12:33:53 +0100

    added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer> This project was coded by Blake Davies, and is
+      <a href="https://github.com/blakebdavies/react-weather-app" target="_blank" rel="noreferrer"> open-sourced </a>on GitHub
+    </footer>
     </div>
   );
 }
