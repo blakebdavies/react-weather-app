66e672df224885e2db5216fdebbbd8aed2f77afe
blakebdavies <81705490+blakebdavies@users.noreply.github.com>
2021-05-23 13:16:12 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer> This project was coded by Blake Davies, and is
       <a href="https://github.com/blakebdavies/react-weather-app" target="_blank" rel="noreferrer"> open-sourced </a>on GitHub
     </footer>
+    </div>
     </div>
   );
 }
