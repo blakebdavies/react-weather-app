bce25f4fb3dcb7114a1e219651220577410f70ea
blakebdavies <81705490+blakebdavies@users.noreply.github.com>
2021-05-23 17:02:22 +0100

    updated icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -13,7 +13,7 @@ export default function Weather(props){
             date: new Date(response.data.dt * 1000),
             description: response.data.weather[0].description,
             humidity: response.data.main.humidity,
-            iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+            iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
             temperature:response.data.main.temp, 
             wind: response.data.wind.speed
             
