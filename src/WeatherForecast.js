d57bbdf70cf9d578e134abc8957ee1899540ab83
blakebdavies <81705490+blakebdavies@users.noreply.github.com>
2021-05-25 20:03:41 +0100

    connected api call with forecast layout

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,32 +1,36 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
+import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props){
+let [loaded, setLoaded] = useState(false);
+let [forecast, setForecast] = useState(null);
 
-    function handleResponse(response){
-       console.log(response.data);
+function handleResponse(response){
+       setForecast(response.data.daily);
+       setLoaded(true);
     }
+
+    if (loaded){
+ return (
+        <div className="WeatherForecast">
+            <div className="row">
+                   <div className="col">
+               <WeatherForecastDay data={forecast.data}/>
+            </div>
+        </div>
+        </div>
+    );
+
+    } else {
     const apiKey="ff72838e9031d5bea109425dc6b40694";
     let longitude=props.coordinates.lon;
     let latitude=props.coordinates.lat;
-    let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units="metric"`;
+    let apiUrl =`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
-    return (
-        <div className="WeatherForecast">
-            <div className="row">
-               <div className="col">
-                    <div className="WeatherForecast-day">Thu</div>
-                  
-                    <WeatherIcon code="01d" size={36} />
-                   
-                    <div className="WeatherForecast-temperatures">
-                    <span className="WeatherForecast-temperatures-max">19°</span>
-                    <span className="WeatherForecast-temperatures-min">10°</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
+
+    return null;
+
+}
 }
