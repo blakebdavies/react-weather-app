31e7e3b0d7f19e5ed94a7456db963aa3af0b2300
blakebdavies <81705490+blakebdavies@users.noreply.github.com>
2021-06-01 21:37:31 +0100

    fixed responsive button

--- src/Weather.js
+++ src/Weather.js
@@ -53,7 +53,7 @@ export default function Weather(props){
         <div className="Weather">
             <form onSubmit={handleSubmit}>
                 <div className="row">
-                    <div className="col-9">
+                    <div className="col-8">
                 <input 
                 type="search" 
                 placeholder="Enter a city..." 
@@ -61,7 +61,7 @@ export default function Weather(props){
                 autoFocus="on" 
                 onChange={handleCityChange}/>
                 </div>
-                <div className="col-3">
+                <div className="col-4">
                 <input type="submit" value="Search" className="btn btn-primary w-100"/>
             </div>
             
